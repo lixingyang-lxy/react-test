3fcb6a2135267c5aaba4e46c1ec9552f7fe8e00e
lixingyang-lxy <1433565248@qq.com>
2021-08-05 10:49:03 +0800

    exact 匹配模式

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Random from '../src/components/Random'
 
 const First = () =>　<p>页面一的内容</p>
+const Home = () =>　<p>首页的内容</p>
 function App() {
   return (
     <Router>
@@ -16,8 +17,12 @@ function App() {
           <div>
             {/* 路由出口 */}
             <Route path="/first" component={First}></Route>
+            {/* <Route path="/" component={Home}></Route> */}
+            <Route exact path="/" component={Home}></Route>
           </div>
           <Link to="/first">页面一</Link>
+          <div>鞥先</div>
+          <Link to="/">首页</Link>
         </header>
         {/* 路由入口 */}
       </div>
