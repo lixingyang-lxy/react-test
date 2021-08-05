6bbfbde297ee42f228d25e703b835642049fcddc
lixingyang-lxy <1433565248@qq.com>
2021-08-05 10:44:41 +0800

    react router base 以及 exact

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import '../src/css/App.css';
-
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import { MousePosition, CatPosition } from "../src/components/Mouse";
-
 import Random from '../src/components/Random'
+
+const First = () =>　<p>页面一的内容</p>
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        {/* <MousePosition a="1"/>  */}
-        {/* <CatPosition b="1"/> */}
-        <Random></Random>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header className="App-header">
+          {/* <MousePosition a="1"/>  */}
+          {/* <CatPosition b="1"/> */}
+          <Random></Random>
+          <h1>路由基础</h1>
+          <div>
+            {/* 路由出口 */}
+            <Route path="/first" component={First}></Route>
+          </div>
+          <Link to="/first">页面一</Link>
+        </header>
+        {/* 路由入口 */}
+      </div>
+    </Router>
   );
 }
 
